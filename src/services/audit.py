59476dad71c0b08be2dd2b0bee59ad0f2c5fa991
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2025-09-08 21:08:38 -0600

    feat: audit service for actions registration

---
+++ src/services/audit.py
@@ -0,0 +1,41 @@
+import inspect
+
+from fastapi import Request
+
+from src.middlewares.exceptions import AppException
+from src.models import AuditLog
+from src.utils.logger import get_logger
+
+logger = get_logger()
+
+
+class AuditService:
+    """Audition logging service."""
+
+    @staticmethod
+    async def register(*args, **kwargs) -> None:
+        """Register actions completed from endpoint and store them."""
+        try:
+            user = kwargs.get("current_user")
+            db = kwargs.get("db")
+            req: Request = kwargs.get("request")
+            action = kwargs.get("action")
+            # data = kwargs.get("data")
+
+            obj_data = {
+                "user_id": user.id or None,
+                "action_performed":action.__qualname__,
+                "affected_module":action.__self__.__class__.__name__,
+                "ip_address":req.headers.get("X-Forwarded-For") or req.client.host,
+                "user_agent":req.headers.get("user-agent"),
+            }
+
+            audit_obj = AuditLog(**obj_data)
+            1/0
+            db.add(audit_obj)
+            await db.commit()
+
+        except Exception as e:
+            db.rollback()
+            logger.error("An unexpected error handling logs has occurred.")
+            logger.error(e)
