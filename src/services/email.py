3f239ea3bf36964529fe8b307e8eaeac8c4d9cd9
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2025-09-13 21:02:58 -0600

    feat: BASE email service send message func

--- src/services/email.py
+++ src/services/email.py
@@ -1,9 +1,16 @@
 """This module handle service for email sending."""
 import os
+from typing import List, Optional
+
+from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
 from pydantic import BaseModel, EmailStr
-from fastapi_mail import ConnectionConfig, FastMail
+
 from src.config.core import core_settings
-from typing import List
+from src.utils.logger import get_logger
+
+
+logger = get_logger()
+
 
 class EmailSchema(BaseModel):
     """Schema model for Email sending."""
@@ -27,8 +34,22 @@ class EmailService:
     )
 
 
-    @staticmethod
-    def send_message() -> None:
+    @classmethod
+    async def send_message(cls, message: Optional[str] = None) -> None:
         """Send email message from FastMail as background task."""
-        print("Mandando mensaje...")
-        print("Enviado")
+        try:
+            print("Mandando mensaje...")
+            print("Enviado")
+            print(message)
+            fm = FastMail(config=cls._conf)
+
+            await fm.send_message(
+                MessageSchema(
+                    recipients=["ochoa.carlos8@outlook.com"],
+                    subject="Test desde pytyhon fastapi",
+                    body="""<p>Hi this test mail, thanks for using Fastapi-mail</p> """,
+                    subtype=MessageType.html
+                )
+            )
+        except Exception as e:
+            logger.error(f"Error enviando email: {e}")
